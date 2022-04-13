40933bad1a2d60cd3601323db2f57ca524f4a745
SamiBegah <91080023+SamiBegah@users.noreply.github.com>
2022-04-13 14:47:59 -0400

    update

--- quiz.js
+++ quiz.js
@@ -1,7 +1,6 @@
 const header = document.querySelector(".header");
 const question = document.querySelector(".text");
 const next = document.querySelector(".next");
-const animator = document.getElementById("animator");
 const choice1 = document.querySelector(".choice1");
 const choice2 = document.querySelector(".choice2");
 const choice3 = document.querySelector(".choice3");
@@ -18,9 +17,92 @@ const questions = {
     choice: ["4", "3", "2", "1"],
     answer: "3",
   },
+  question3: {
+    question: "How many islands does Sweden have?",
+    choice: ["127", "75,263", "153,900", "267,570"],
+    answer: "267,570",
+  },
+  question5: {
+    question: "How long is the human circulatory system?",
+    choice: ["500 miles", "200 meters", "60,000 miles", "25,000 miles"],
+    answer: "60,000 miles",
+  },
+  question6: {
+    question: "Where the world's first animated film was made?",
+    choice: ["USA", "France", "Argentina", "Mexico"],
+    answer: "Argentina",
+  },
+  question7: {
+    question: "How much calories do blue whales eat in one mouthful?",
+    choice: [
+      "457,000 calories",
+      "265,000 calories",
+      "635,000 calories",
+      "85,000 calories",
+    ],
+    answer: "457,000 calories",
+  },
+  question8: {
+    question:
+      "How many calories do humans burn after banging their head against a wall for one hour?",
+    choice: ["50 calories", "150 calories", "300 calories", "75 calories"],
+    answer: "150 calories",
+  },
+  question9: {
+    question:
+      "What percentage of american adults think that chocolate milk comes from brown cows?",
+    choice: ["2%", "5%", "15%", "7%"],
+    answer: "7%",
+  },
+  question10: {
+    question: "In what country was invented the hawaiian pizza?",
+    choice: ["Italia", "USA", "Canada", "Chile"],
+    answer: "Canada",
+  },
+  question11: {
+    question: "What percentage of the ocean was explored by humans?",
+    choice: ["50%", "35%", "5%", "10%"],
+    answer: "5%",
+  },
+  question12: {
+    question: "What is illegal in Switzerland?",
+    choice: [
+      "Owning just one guinea pig",
+      "Paying taxes",
+      "Not having a bank account",
+      "Eating outside",
+    ],
+    answer: "Owning just one guinea pig",
+  },
 };
 
-lives = 3;
+const arrAnimators = ["animatorHi", "animatorO", "animatorX", "animatorQ"];
+
+function animatorShow(letter) {
+  for (const animatorSelected of arrAnimators) {
+    if (animatorSelected === `animator${letter}`) {
+      console.log(`show ${animatorSelected}`);
+      document.getElementById(`animator${letter}`).style.animationName = "show";
+      document.getElementById(`animator${letter}`).style.animationDuration =
+        "0.5s";
+      document.getElementById(`animator${letter}`).style.animationFillMode =
+        "forwards";
+    } else {
+      console.log(`hide ${animatorSelected}`);
+      document.getElementById(`animator${letter}`).style.animationName = "hide";
+      document.getElementById(`animator${letter}`).style.animationDuration =
+        "0.5s";
+      document.getElementById(`animator${letter}`).style.animationFillMode =
+        "forwards";
+      console.log(document.getElementById(`animator${letter}`).style);
+    }
+  }
+}
+
+let lives = 3;
+let gameOn = false;
+
+animatorShow("Hi");
 
 function randomQuestion(obj) {
   const keys = Object.keys(obj);
@@ -55,13 +137,12 @@ choice1.addEventListener("click", function () {
     next.style.animationDuration = "0.5s";
     next.style.animationFillMode = "forwards";
     next.style.cursor = "pointer";
+    animatorShow("X");
   } else {
+    animatorShow("O");
     choice1.style.backgroundColor = "red";
     header.textContent = "Wrong answer!";
     header.style.color = "red";
-    animator.src =
-      "https://assets1.lottiefiles.com/private_files/lf30_qynkjvnj.json";
-    console.log(animator);
     lives -= 1;
   }
 });
@@ -74,12 +155,13 @@ choice2.addEventListener("click", function () {
     next.style.animationName = "show";
     next.style.animationDuration = "0.5s";
     next.style.animationFillMode = "forwards";
+    animatorShow("X");
   } else {
+    animatorShow("O");
     choice2.style.backgroundColor = "red";
     header.textContent = "Wrong answer!";
     header.style.color = "red";
-    animator.src =
-      "https://assets1.lottiefiles.com/private_files/lf30_qynkjvnj.json";
+
     lives -= 1;
   }
 });
@@ -91,12 +173,12 @@ choice3.addEventListener("click", function () {
     next.style.animationName = "show";
     next.style.animationDuration = "0.5s";
     next.style.animationFillMode = "forwards";
+    animatorShow("X");
   } else {
+    animatorShow("O");
     choice3.style.backgroundColor = "red";
     header.textContent = "Wrong answer!";
     header.style.color = "red";
-    animator.src =
-      "https://assets1.lottiefiles.com/private_files/lf30_qynkjvnj.json";
     lives -= 1;
   }
 });
@@ -108,18 +190,20 @@ choice4.addEventListener("click", function () {
     next.style.animationName = "show";
     next.style.animationDuration = "0.5s";
     next.style.animationFillMode = "forwards";
+    animatorShow("X");
   } else {
+    animatorShow("O");
     choice4.style.backgroundColor = "red";
     header.textContent = "Wrong answer!";
     header.style.color = "red";
-    animator.src =
-      "https://assets1.lottiefiles.com/private_files/lf30_qynkjvnj.json";
     lives -= 1;
   }
 });
 
 next.addEventListener("click", function () {
   delete questions[actualQuestion];
+  gameOn = false;
+  animatorShow("Q");
   randomize();
   next.style.animationName = "hide";
   next.style.animationDuration = "0.5s";
